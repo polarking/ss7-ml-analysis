fb22e6f29ecd0c3c149afbc315e0924cf0cd99bc
Kristoffer Jensen <jensenkris91@gmail.com>
2016-05-05 19:14:00 +0200

    Normalize input before clustering

--- src/main/scala-2.10/Main.scala
+++ src/main/scala-2.10/Main.scala
@@ -4,19 +4,20 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.feature.Normalizer
 import org.elasticsearch.spark._
 import org.apache.spark.mllib.clustering.StreamingKMeans
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
-import org.cloudera.spark.streaming.kafka.KafkaWriter._
 
-/**
-  * Created by kristoffer on 26.04.2016.
-  */
 object Main {
   def main(args: Array[String]): Unit = {
-    val master = "spark://cruncher:7077"
+    val master = args(0)
     val appName = "SS7MLAnalysis"
+
+    val user = args(1)
+    val pass = args(2)
+    val trainingDataPath = args(3) //Directory of the form: file:///home/kristoffer/spark-testing-data
 
     val conf = new SparkConf()
     conf.setAppName(appName)
@@ -24,29 +25,33 @@ object Main {
     conf.set("spark.cores.max", "8")
 
     // Elasticsearch configuration.
+    conf.set("es.resource", "ss7-ml-results/results")
     conf.set("es.index.auto.create", "true")
+    conf.set("es.net.http.auth.user", user)
+    conf.set("es.net.http.auth.pass", pass)
 
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(10))
 
     // Creating a K Means model.
-    val numDimensions = 4
+    val numDimensions = 3
     val numClusters = 2
     val model = new StreamingKMeans()
       .setK(numClusters)
       .setDecayFactor(1.0)
       .setRandomCenters(numDimensions, 0.0)
 
+    //Creating a normalizer object that normalizes features with L^2 norm.
+    val normalizer = new Normalizer()
+
     //"Train" K Means model on existing data contained in regular file
-    val trainingDataPath = "file:///home/kristoffer/spark-testing-data"
     val trainingData = ssc.textFileStream(trainingDataPath).map(line => {
       val split = line.split(",")
       val byteLength = split(1).toDouble
       val lastUpdate = split(2).toDouble
       val travelDist = split(3).toDouble
-      val newLac = split(4).toDouble
 
-      Vectors.dense(byteLength, lastUpdate, travelDist, newLac)
+      normalizer.transform(Vectors.dense(byteLength, lastUpdate, travelDist))
     })
     model.trainOn(trainingData)
 
@@ -59,15 +64,14 @@ object Main {
 
     //Process each message from Kafka and extract features
     val processedMessages = messages.flatMap(_._2.split("\n")).map(message => {
-      //Expected features: timeEpoch,byteLength,lastUpdate,travelDist,newLac
+      //Expected features: label,byteLength,lastUpdate,travelDist
       val split = message.split(",")
-      val timeEpoch = split(0).toDouble
+      val label = split(0).toInt
       val byteLength = split(1).toDouble
       val lastUpdate = split(2).toDouble
       val travelDist = split(3).toDouble
-      val newLac = split(4).toDouble
 
-      LabeledPoint(timeEpoch, Vectors.dense(byteLength, lastUpdate, travelDist, newLac))
+      LabeledPoint(label, normalizer.transform(Vectors.dense(byteLength, lastUpdate, travelDist)))
     })
 
     //Make prediction on new input
@@ -76,12 +80,11 @@ object Main {
       val rddCollect = rdd.collect()
       rddCollect.foreach(prediction => {
         //Save cluster assignment and label for sending to ES
-        val esMap = Map("label" -> prediction._1)
-        val esMap2 = Map("cluster" -> prediction._2)
+        val esMap = Map("label" -> prediction._1, "cluster" -> prediction._2)
 
         //Save cluster assignment in elasticsearch for visualization and preprocessing
-        val esRDD = sc.makeRDD(Seq(esMap, esMap2))
-        esRDD.saveToEs("ss7-ml-results/clustering")
+        val esRDD = sc.makeRDD(Seq(esMap))
+        esRDD.saveToEs("ss7-ml-results/results")
       })
     })
 
